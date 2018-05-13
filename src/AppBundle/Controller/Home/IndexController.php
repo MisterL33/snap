1babc1018993a1f90dbb4ca0b733b71c6d3483f7
Laurent JULIENNE <laurent.julienne@ynov.com>
2018-05-13 22:58:49 +0200

    seo v2

--- src/AppBundle/Controller/Home/IndexController.php
+++ src/AppBundle/Controller/Home/IndexController.php
@@ -12,83 +12,11 @@ class IndexController extends Controller
 
     public function indexAction(Request $request)
     {
-        $em = $this->getDoctrine()->getEntityManager();
-        $success = null;
-        $failed = null;
-        $type = null;
-        $location = null;
         
-        if($request->request->get('pseudo') !== null){
-            $pseudo = $request->request->get('pseudo');
-        }else{
-            $pseudo = 'TropicalGuy33';
-        }
-
-        if($request->request->get('type') !== null){
-            $type = $request->request->get('type');
-        }else{
-            $type = 1;
-        }
-
-        if($request->request->get('locations') !== null){
-            $location = $request->request->get('locations');
-        }else{
-            $location = 95;
-        }
-
-
-
-        if (isset($location) && isset($pseudo) && isset($type))
-        {
-       
-            
-            $selectedLocation = $em->getRepository('AppBundle:Location')->find($location);
-
-            if ($this->validatePseudo($pseudo) == false)
-            {
-                $user = new User();
-
-                $user->setLogin($pseudo);
-                $user->setLocation($selectedLocation);
-                $user->setHot($type);
-                $em->persist($user);
-                $success = 'Tu est maintenant dans la liste';
-                $em->flush();
-    
-            }
-            else
-            {
-                $failed = 'Tu est déjà dans la liste';
-          
-            }
-        }
-
-
-        $allLocation = $em->getRepository('AppBundle:Location')->findAll();
-        //      var_dump($selectedLocation);
-        //$user->setLocation($location);
-        // replace this example code with whatever you need
         return $this->render('Home/index.html.twig', array(
-                    'locations' => $allLocation,
-                    'success' => $success,
-                    'failed' => $failed
+
         ));
     }
 
-    public function validatePseudo($pseudo)
-    {
-
-        $em = $this->getDoctrine()->getEntityManager();
-        $exist = false;
-        $user = $em->getRepository('AppBundle:User')->findBy(array('login' => $pseudo));
-
-        if ($user)
-        {
-            $exist = true;
-            return true;
-        }
-
-        return false;
-    }
 
 }
